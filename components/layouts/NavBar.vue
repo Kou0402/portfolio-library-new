d7dd9346090e25d083e7ed12d53d96306a017835
kou0402 <kou.4869.biz@gmail.com>
2019-10-27 22:18:14 +0900

    style: add NavBar component

---
+++ components/layouts/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="nav-bar">
+    Nav-bar
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.nav-bar {
+  background-color: $main-color1;
+  height: 60px;
+}
+</style>
