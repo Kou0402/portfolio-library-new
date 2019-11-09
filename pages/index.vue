937909ba20cac8330cd93293677e227cbe0d0aa3
kou0402 <kou.4869.biz@gmail.com>
2019-11-09 14:21:15 +0900

    style: add ImageCard.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <section class="home">
     <HomeHero></HomeHero>
     <OrderArea></OrderArea>
+    <PortfolioArea></PortfolioArea>
   </section>
 </template>
 
 <script>
 import HomeHero from '~/components/page/home/HomeHero.vue'
 import OrderArea from '~/components/page/home/OrderArea.vue'
+import PortfolioArea from '~/components/page/home/PortfolioArea.vue'
 
 export default {
   components: {
     HomeHero,
-    OrderArea
+    OrderArea,
+    PortfolioArea
   }
 }
 </script>
