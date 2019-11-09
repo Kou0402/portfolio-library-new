40bca3d4e35026a34bc0e9d8b5128d708ebf18be
kou0402 <kou.4869.biz@gmail.com>
2019-11-09 19:44:05 +0900

    style: add PortfolioCard.vue

--- components/page/home/PortfolioArea.vue
+++ components/page/home/PortfolioArea.vue
@@ -1,5 +1,16 @@
 <template>
   <section class="portfolio-area">
     PortfolioArea
+    <PortfolioCard></PortfolioCard>
   </section>
 </template>
+
+<script>
+import PortfolioCard from '~/components/page/home/PortfolioCard.vue'
+
+export default {
+  components: {
+    PortfolioCard
+  }
+}
+</script>
---
+++ components/page/home/PortfolioCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <a href="https://portfolio-library.com" class="portfolio-card">
+    <img src="sample.png" alt="" class="image" />
+    <a href="https://twitter.com/ko_u_0402" class="icon">
+      <i class="fab fa-twitter"></i>
+    </a>
+    <p class="description">
+      テストテストテストテストテストテストテストテストテストテストテスト
+    </p>
+    <div class="favorite"><span class="star">★</span>999</div>
+  </a>
+</template>
+
+<style lang="scss" scoped>
+.portfolio-card {
+  display: block;
+  position: relative;
+  width: 300px;
+  height: 250px;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  > .image {
+    width: 300px;
+    height: 200px;
+  }
+  > .icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 150px;
+    left: 250px;
+    width: 40px;
+    height: 40px;
+    font-size: 24px;
+    background-color: #1da1f2;
+    color: #fff;
+    border-radius: 50%;
+  }
+  > .description {
+    font-size: 1.6rem;
+    line-height: 2.4rem;
+    padding: 0 4px;
+  }
+  > .favorite {
+    position: absolute;
+    font-size: 1.6rem;
+    top: 230px;
+    left: 256px;
+    > .star {
+      color: yellow;
+    }
+  }
+}
+</style>
